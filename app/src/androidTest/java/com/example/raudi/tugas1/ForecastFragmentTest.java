33df1e1974b99104cd8d0210876f45d7253e0030
Praditya Raudi <raudiradz@gmail.com>
2016-12-04 18:09:06 +0700

    Tugas 1 fin

---
+++ app/src/androidTest/java/com/example/raudi/tugas1/ForecastFragmentTest.java
@@ -0,0 +1,10 @@
+package com.example.raudi.tugas1;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by raudi on 12/4/16.
+ */
+public class ForecastFragmentTest {
+
+}
---
+++ app/src/main/java/com/example/raudi/tugas1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.raudi.tugas1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+}
